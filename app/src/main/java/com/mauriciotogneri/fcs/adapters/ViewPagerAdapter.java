6924c603a5140b3ee9e4c4dd710f64775f6e6219
mtogneri <mtogneri@openwt.com>
2016-10-09 16:42:53 +0200

    Added tabs in ground version

---
+++ app/src/main/java/com/mauriciotogneri/fcs/adapters/ViewPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.mauriciotogneri.fcs.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter
+{
+    private final List<Fragment> fragments = new ArrayList<>();
+    private final List<String> names = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager)
+    {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount()
+    {
+        return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment, String title)
+    {
+        fragments.add(fragment);
+        names.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position)
+    {
+        return names.get(position);
+    }
+}
--- app/src/main/java/com/mauriciotogneri/fcs/ground/GroundActivity.java
+++ app/src/main/java/com/mauriciotogneri/fcs/ground/GroundActivity.java
@@ -1,12 +1,15 @@
 package com.mauriciotogneri.fcs.ground;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 import com.mauriciotogneri.fcs.R;
+import com.mauriciotogneri.fcs.adapters.ViewPagerAdapter;
 
-public class GroundActivity extends FragmentActivity
+public class GroundActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,5 +19,23 @@ public class GroundActivity extends FragmentActivity
         setContentView(R.layout.ground_main);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new OneFragment(), "TWO");
+        adapter.addFragment(new OneFragment(), "THREE");
+
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.setCurrentItem(0);
+        viewPager.setAdapter(adapter);
     }
 }
